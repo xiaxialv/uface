9a8c07d235720131292563281abfc011783e9205
Sidney <xiaotian.chen@quvideo.com>
2019-01-08 11:34:35 +0800

    update

--- src/main/java/com/zaozao/App.java
+++ src/main/java/com/zaozao/App.java
@@ -1,13 +1,51 @@
 package com.zaozao;
 
+import com.alibaba.fastjson.JSON;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication(scanBasePackages = {"com.zaozao"}, exclude = DataSourceAutoConfiguration.class)
+@RestController
+public class App {
+
+    private static final String APP_ID = "64CCC7F9ABB44F598DC260A1B8629089";
+    private static final String APP_KEY = "D53F039BFA2140FB9840DA1095B3BE0E";
+    private static final String APP_SECRET = "3EC20F59D66E40CEA4AF6A66156A9570";
+
+    @RequestMapping(value = "/getToken", method = {RequestMethod.GET})
+    public String getToken() throws Exception {
+        long timestamp = System.currentTimeMillis();
+        //文档 : http://developer.uface.uni-ubi.com/document/18011
+        Map<String, Object> map = new HashMap<>();
+        map.put("appId", APP_ID);
+        map.put("appKey", APP_KEY);
+        map.put("timestamp", timestamp + "");
+
+        //获取sign
+        //MD5-32(appKey+timestamp+appSecret)，MD5加密，32位小写
+        String sign =
+                EncryptUtil.md532(APP_KEY + timestamp + APP_SECRET);
+
+        map.put("sign", sign);
+
+        String result =
+                HttpRequestUtil.post("http://gs-api.uface.uni-ubi.com/v1/" + APP_ID + "/auth", JSON.toJSONString(map));
+        System.out.println(result);
+        return "xiaxia.html";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
---
+++ src/main/java/com/zaozao/EncryptUtil.java
@@ -0,0 +1,35 @@
+package com.zaozao;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Sidney 2019-01-08.
+ */
+public class EncryptUtil {
+    public static String md532(String str) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String result = "";
+
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        md5.update((str).getBytes("UTF-8"));
+        byte b[] = md5.digest();
+
+        int i;
+        StringBuffer buf = new StringBuffer("");
+
+        for(int offset=0; offset<b.length; offset++){
+            i = b[offset];
+            if(i<0){
+                i+=256;
+            }
+            if(i<16){
+                buf.append("0");
+            }
+            buf.append(Integer.toHexString(i));
+        }
+
+        result = buf.toString();
+        return result;
+    }
+}
