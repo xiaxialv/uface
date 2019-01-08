65cf206abafc534a23ff01f0a477d2bbca77d06a
xiaxialv <516318467@qq.com>
2019-01-08 17:35:54 +0800

    lvlvperson commit

--- src/main/java/com/zaozao/App.java
+++ src/main/java/com/zaozao/App.java
@@ -18,9 +18,6 @@ public class App {
 
     @Autowired
     private TokenService tokenService;
-    private static final String APP_ID = "64CCC7F9ABB44F598DC260A1B8629089";
-    private static final String APP_KEY = "D53F039BFA2140FB9840DA1095B3BE0E";
-    private static final String APP_SECRET = "3EC20F59D66E40CEA4AF6A66156A9570";
 
     @RequestMapping(value = "/getToken", method = {RequestMethod.GET})
     public String getToken() throws Exception {
---
+++ src/main/java/com/zaozao/service/PersonService.java
@@ -0,0 +1,38 @@
+package com.zaozao.service;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.zaozao.util.HttpRequestUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sidney 2019-01-08.
+ */
+@Service
+public class PersonService {
+    @Autowired
+    private TokenService tokenService;
+    private static final String APP_ID = "64CCC7F9ABB44F598DC260A1B8629089";
+
+    public String getPerson(String guid) {
+        Map<String, String> map = new HashMap<>();
+        String token = tokenService.getToken();
+        map.put("appId", APP_ID);
+        map.put("token", token);
+        if (guid!=null||!guid.equals("")) {
+            map.put("guid",guid);
+        }
+        System.out.println(JSON.toJSONString(map));
+        //URL：http://gs-api.uface.uni-ubi.com/v1/{appId}/person/{guid}，method：GET
+        String result =
+                HttpRequestUtil.postParamUrl("http://gs-api.uface.uni-ubi.com/v1/" + APP_ID + "/person/"+guid, map);
+        JSONObject jsonObject = JSON.parseObject(result);
+        System.out.println(jsonObject);
+        return result;
+    }
+
+}
